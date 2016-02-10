df3543dab3b96766b701fb79f6fba2d7dbedc78e
muffasa <ohadgalor1990@gmail.com>
2016-02-10 13:29:28 +0200

    meteor package that is transformed into npm package js files and readme added

---
+++ transformed_package_js/workbook.js
@@ -0,0 +1,33 @@
+var XLSX = require('xlsx');
+var XLS = require('xlsjs');
+
+var Workbook = function (fileType) {
+  this.fileType = fileType;
+
+  this.SheetNames = [];
+  this.Sheets = {};
+};
+
+Workbook.prototype.addSheet = function (sheetName, sheet) {
+  this.SheetNames.push(sheetName);
+  this.Sheets[sheetName] = sheet;
+};
+
+Workbook.prototype.writeToFile = function (filePath) {
+  if (this.fileType === 'xlsx') {
+    return XLSX.writeFile(this, filePath);
+  }
+  else if (this.fileType == 'xls') {
+    return XLS.writeFile(this, filePath);
+  }
+};
+
+Workbook.prototype.write = function (wopts) {
+    if (this.fileType === 'xlsx') {
+        return XLSX.write(this, wopts);
+    }
+    else if (this.fileType == 'xls') {
+        return XLS.write(this, wopts);
+    }
+};
+exports.Workbook = Workbook;
